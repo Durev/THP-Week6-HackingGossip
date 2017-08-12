afab6ee8a3a95974ca62af174c1a671a2ba69bc3
Durev <vincent.durewski@gmail.com>
2017-08-12 18:47:10 +0200

    Add gossips index on the homepage

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -10,4 +10,22 @@ class GossipsController < ApplicationController
   	@gossips = Gossip.all
   end
 
+  def create
+  	chosen_corsair = Corsair.find_by(username: gossip_params[])
+  	@gossip = chosen_corsair.gossips.build(gossip_params)
+  	if @gossip.save
+  		flash[:success] = "New gossip added !"
+  		redirect_to root_path
+  	else
+  		#flash[:fail] ?
+  		render 'gossips#new'
+  	end
+  end
+
+  private
+
+  	def gossip_params
+  		params.require(:gossip).permit(:content)
+  	end
+
 end
